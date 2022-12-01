d6c206285ddbca8c9ca69950b41fb1a9834ed980
Daniel Gitau <gitaud959@gmail.com>
2022-12-01 21:13:09 +0300

    added studentgradegenerator function

---
+++ studentGradeGenerator/index.js
@@ -0,0 +1,16 @@
+const studentMarks = prompt('Please enter your marks');
+
+function studentGradeGenerator(marks) {
+  if (marks > 79 ){
+    return 'A';
+  }else if (marks >= 60 && marks <= 79){
+    return 'B';
+  }else if (marks >= 50 && marks <= 59){
+    return 'C';
+  }else if (marks >= 40 && marks <= 49){
+    return 'D';
+  }else if(marks < 40){
+    return 'E';
+  }
+}
+console.log(studentGradeGenerator(studentMarks));
